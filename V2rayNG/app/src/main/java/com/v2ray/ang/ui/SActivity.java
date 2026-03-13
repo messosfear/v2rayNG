aeee31b3c7a495105156fc9d45d78d0d5272a4d6
Collins Mucho <72305974+sleepchild@users.noreply.github.com>
2026-03-13 14:40:16 +0200

    t2

---
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/SActivity.java
@@ -0,0 +1,23 @@
+package com.v2ray.ang.ui
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * @Author sleepchild
+ * @Date 2026/03/13 14:39
+ */
+public class SActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this,"SActivity", 500).show();
+    }
+
+
+
+
+
+}
